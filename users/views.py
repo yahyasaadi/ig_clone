955cdc48ca0abc78369a6e0f54e7e9906982f11f
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-05 11:14:58 +0300

    Updated the user views with register form

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegisterForm
 
 # Create your views here.
+def register(request):
+    form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
