a14671bee290c35be969a976d179ea39138a9665
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-07 10:16:47 +0300

    Updated the URLs

--- ig_project/urls.py
+++ ig_project/urls.py
@@ -20,7 +20,7 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register, name='register'),
+    path('', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('', include('photos.urls'))
 ]
