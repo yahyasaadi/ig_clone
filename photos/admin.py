f3ed622fcf3cddf7f7138e8a00fcc311d3c54bf1
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-07 10:24:28 +0300

    Registered the models

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Photo, Comment, Like
 
 # Register your models here.
+admin.site.register(Photo)
+admin.site.register(Comment)
+admin.site.register(Like)
