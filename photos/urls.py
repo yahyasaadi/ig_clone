01d05a774e8fcd0f4abb54ae34575fbf44317b96
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-11 18:01:48 +0300

    Added a view to search for a user

--- photos/urls.py
+++ photos/urls.py
@@ -1,12 +1,25 @@
 from django.urls import path
-from .views import PhotoListView, PhotoCreateView, PhotoDetailView, PhotoUpdateView, PhotoDeleteView
+from .views import (PhotoListView,
+                    PhotoCreateView,
+                    PhotoDetailView,
+                    PhotoUpdateView,
+                    PhotoDeleteView,
+                    UserPhotoListView,
+                    LikeView,
+                    CommentCreateView
+)
 from . import views
 
 urlpatterns = [
     # path('home/', views.home, name='home-page')
     path('home/', PhotoListView.as_view(), name='home-page'),
+    path('user/<str:username>', UserPhotoListView.as_view(), name='user-posts'),
     path('photo/<int:pk>/', PhotoDetailView.as_view(), name='photo-detail'),
     path('photo/new/', PhotoCreateView.as_view(), name='photo-create'),
     path('photo/<int:pk>/update/', PhotoUpdateView.as_view(), name='photo-update'),
     path('photo/<int:pk>/delete/', PhotoDeleteView.as_view(), name='photo-delete'),
+    path('search/', views.search, name='search'),
+    path('like/<int:pk>/', LikeView, name='like-photo'),
+    # path('comment/<int:pk>/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<slug:username>/new', CommentCreateView.as_view(), name='comment-create'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Photo
+from .models import Photo, Comment, Like
 
 # Create your views here.
 def home(request):
@@ -14,6 +17,17 @@ class PhotoListView(ListView):
     model = Photo
     ordering = ['-date_posted']
     
+
+class UserPhotoListView(ListView):
+    model = Photo # blog/post_list.html requires this template to run
+    template_name = 'blog/user_posts.html' # Or specify the template to use here
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Photo.objects.filter(owner=user).order_by('-date_posted')
+
+
 
 class PhotoDetailView(DetailView):
     model = Photo
@@ -52,3 +66,36 @@ class PhotoDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == photo.owner:
             return True
         return False
+
+
+
+# Search method
+def search(request):
+	if request.method == "POST":
+		searched = request.POST['searched']
+		pics = Photo.objects.filter(owner__username__contains=searched)
+	
+		return render(request, 'photos/search.html', {'pics':pics})
+	else:
+		return render(request, 'photos/search.html')
+
+
+
+
+# Comments Views
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment 
+    fields = ['comment']
+
+    def form_valid(self, form):
+        photo = self.get_object()
+        form.instance.photo = photo
+        form.instance.owner = self.request.user
+        # form.save()
+        return super().form_valid(form)
+
+
+def LikeView(request, pk):
+    photo = get_object_or_404(Photo, id=request.POST.get('img_id'))
+    photo.like.add(request.user)
+    return HttpResponseRedirect(reverse('photo-detail', args=[str(pk)]))
