ca1ef86819caeb5c82aa6bb8dbed8a71b805e011
yahyasaadi <yahyasaadi9219@gmail.com>
2022-03-12 23:26:56 +0300

    Add environment variables

--- ig_project/settings.py
+++ ig_project/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&m2*frxaadhs=^kt*el=*afeysi4j+4mfh(8$b-$w31gk+&x_h'
+# SECRET_KEY = 'django-insecure-&m2*frxaadhs=^kt*el=*afeysi4j+4mfh(8$b-$w31gk+&x_h'
+SECRET_KEY = os.environ.get('SECRET_KEY_DJ')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
