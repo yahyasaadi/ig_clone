ed68115fa698c0bd0bb5274260bb4aaec754a9ce
yahyasaadi <yahyasaadi9219@gmail.com>
2022-04-15 15:50:44 +0300

    Changed settings

--- ig_project/settings.py
+++ ig_project/settings.py
@@ -120,10 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
 
 
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
